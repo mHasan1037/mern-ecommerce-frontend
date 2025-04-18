c615e457d3a9390deaf2ba62ca4f1f1dd7d281d4
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-04-18 14:27:31 +0600

    Added form for the new product

---
+++ app/admin/category/page.tsx
@@ -0,0 +1,13 @@
+import AdminSidebar from "@/components/adminSidebar";
+import React from "react";
+
+const Category = () => {
+  return (
+    <div className="adminMainSection">
+      <AdminSidebar />
+      <h1>Category</h1>
+    </div>
+  );
+};
+
+export default Category;
--- components/buttons/ConfirmButton/index.tsx
+++ components/buttons/ConfirmButton/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import styles from './ConfirmButton.module.css';
+import React from "react";
+import styles from "./ConfirmButton.module.css";
 
 interface ConfirmButtonProps {
-    buttonText: String;
-    type?: "button" | "submit" | "reset";
+  buttonText: String;
+  type?: "button" | "submit" | "reset";
+  onclick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const ConfirmButton: React.FC<ConfirmButtonProps> = ({ buttonText, type = "button" }) => {
+const ConfirmButton: React.FC<ConfirmButtonProps> = ({
+  buttonText,
+  type = "button",
+  onclick,
+}) => {
   return (
-    <button type={type} className={styles.buttonStyle}>{buttonText}</button>
-  )
-}
+    <button type={type} className={styles.buttonStyle} onClick={onclick}>
+      {buttonText}
+    </button>
+  );
+};
 
-export default ConfirmButton
+export default ConfirmButton;

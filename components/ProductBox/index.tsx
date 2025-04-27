9d3d30f6bfeb7549e3d5704ca527db2dd1e50c42
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-04-27 12:06:42 +0600

    Added product price and ratings in the display

--- components/ProductBox/index.tsx
+++ components/ProductBox/index.tsx
@@ -8,7 +8,7 @@ interface ProductBoxProps {
 
 const ProductBox: React.FC<ProductBoxProps> = ({ product }) => {
   return (
-    <div key={product._id}>
+    <div key={product._id} className="cursor-pointer">
       <Image
         src={product.images[0]?.url}
         width={100}
@@ -16,6 +16,10 @@ const ProductBox: React.FC<ProductBoxProps> = ({ product }) => {
         alt={product.name}
       />
       <p>{product.name}</p>
+      <div className="flex justify-between">
+        <p>{product.price}</p>
+        <p>{product.ratings?.average}</p>
+      </div>
     </div>
   );
 };

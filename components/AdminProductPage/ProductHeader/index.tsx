b5f09383cf05d8635d7f15eaab01179fee3b07a7
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-05-04 20:55:52 +0600

    Search option in admin has been added

--- components/AdminProductPage/ProductHeader/index.tsx
+++ components/AdminProductPage/ProductHeader/index.tsx
@@ -8,21 +8,20 @@ import {
   setSelectedCategory,
 } from "@/redux/slices/categorySlice";
 import { fetchProducts } from "@/redux/slices/productSlice";
+import { useDebounce } from "@/utils/useDebounce";
 
 const ProductHeader = () => {
   const router = useRouter();
   const dispatch = useAppDispatch();
   const [isFeatured, setIsFeatured] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const selectedCategory = useAppSelector(
     (state) => state.categories.selectedCategory
   );
   const { categories, loading, error } = useAppSelector(
     (state) => state.categories
   );
-
-  useEffect(() => {
-    dispatch(fetchCategories());
-  }, [dispatch]);
+  const debouncedSearch = useDebounce(searchTerm, 400);
 
   const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     dispatch(setSelectedCategory(e.target.value));
@@ -33,13 +32,15 @@ const ProductHeader = () => {
   };
 
   useEffect(() => {
+    dispatch(fetchCategories());
     dispatch(
       fetchProducts({
         category: selectedCategory ?? undefined,
         is_featured: isFeatured ? true : undefined,
+        search: debouncedSearch.trim() || undefined,
       })
     );
-  }, [selectedCategory, isFeatured, dispatch]);
+  }, [selectedCategory, isFeatured, dispatch, debouncedSearch]);
 
   return (
     <div className="flex justify-between items-start w-full mb-10">
@@ -71,6 +72,13 @@ const ProductHeader = () => {
         <input
           type="text"
           placeholder="Search product"
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setSearchTerm(searchTerm)
+            }
+          }}
           className="px-3 rounded-md focus:outline-none"
         />
         <ConfirmButton

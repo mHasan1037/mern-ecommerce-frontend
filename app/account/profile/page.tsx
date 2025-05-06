f732d02001f96e5c4e7ba0780c1d17d86fdcd38a
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-05-06 12:09:35 +0600

    Added user profile data

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+"use client"
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { loadUser } from '@/redux/slices/authSlice';
+import React, { useEffect } from 'react'
 
 const Profile = () => {
+  const dispatch = useAppDispatch();
+  const {user} = useAppSelector((state) => state.auth);
+
+  useEffect(()=>{
+     dispatch(loadUser())
+  }, [dispatch])
+
   return (
-    <div>Profile</div>
+    <div>
+      <h1>User name: {user?.name}</h1>
+      <h1>Email: {user?.email}</h1>
+      {user?.isVerified && <h2>Verified: Yes</h2>}
+      {user?.isAdmin && <h2>Role: Admin</h2>}
+      {user?.totalSpent && <h2>Total amount ordered: {user.totalSpent}</h2>}
+      {user?.totalDelivedOrder && <h2>Total orders: {user.totalDelivedOrder}</h2>}
+      {user?.recentOrder && <h2>Recent order: {user?.recentOrder.orderItems[0]?.name}</h2>}
+      {user?.totalDelivedOrder && <h2>Total orders: {user.totalDelivedOrder}</h2>}
+      {user?.totalCancelledOrders && <h2>Total orders: {user.totalCancelledOrders}</h2>}
+    </div>
   )
 }
 
--- redux/slices/authSlice.ts
+++ redux/slices/authSlice.ts
@@ -1,92 +1,116 @@
 import axiosInstance from "@/utils/axiosInstance";
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 
-interface User{
-    id: string;
-    email: string;
-    name: string;
-    isVerified: boolean;
-    isAdmin: boolean;
+interface ProductSummary {
+  _id: string;
+  name: string;
+}
+
+interface RecentOrder {
+  id: string;
+  status: string;
+  placedAt: string;
+  orderItems: ProductSummary[];
+}
+
+interface User {
+  id: string;
+  email: string;
+  name: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+  recentOrder?: RecentOrder;
+  totalCancelledOrders?: number;
+  totalDelivedOrder?: number;
+  totalSpent?: string;
 }
 
 interface AuthState {
-    user: User | null;
-    accessToken: string | null;
-    refreshToken: string | null;
-    isAuthenticated: boolean;
-    passwordChangeStatus?: "idle" | "loading" | "succeeded" | "failed";
-    passwordChangeError?: string | null;
+  user: User | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+  isAuthenticated: boolean;
+  passwordChangeStatus?: "idle" | "loading" | "succeeded" | "failed";
+  passwordChangeError?: string | null;
 }
 
 const initialState: AuthState = {
-    user: null,
-    accessToken: null,
-    refreshToken: null,
-    isAuthenticated: false,
-    passwordChangeStatus: "idle",
-    passwordChangeError: null,
-}
+  user: null,
+  accessToken: null,
+  refreshToken: null,
+  isAuthenticated: false,
+  passwordChangeStatus: "idle",
+  passwordChangeError: null,
+};
 
 export const loadUser = createAsyncThunk(
-    "auth/loadUser",
-    async (_, thunkAPI) =>{
-        try{
-           const res = await axiosInstance.get('/api/user/me', {
-            withCredentials: true
-           });
-           return {
-            user: res.data.user,
-            isAuthenticated: res.data.is_auth
-           }
-        }catch (error){
-            return thunkAPI.rejectWithValue("Not authenticated");
-        }
+  "auth/loadUser",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axiosInstance.get("/api/user/me", {
+        withCredentials: true,
+      });
+      return {
+        user: res.data.user,
+        isAuthenticated: res.data.is_auth,
+      };
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Not authenticated");
     }
-)
+  }
+);
 
 export const changePassword = createAsyncThunk(
-    "auth/changePassword",
-    async (formData: {password: string; password_confirmation: string}, thunkAPI) =>{
-        try{
-           const res = await axiosInstance.post('/api/user/change-password', formData, {
-            withCredentials: true
-           });
-           return res.data;
-        }catch(error: any){
-           return thunkAPI.rejectWithValue(error.response?.data?.message || "Password change failed")
+  "auth/changePassword",
+  async (
+    formData: { password: string; password_confirmation: string },
+    thunkAPI
+  ) => {
+    try {
+      const res = await axiosInstance.post(
+        "/api/user/change-password",
+        formData,
+        {
+          withCredentials: true,
         }
+      );
+      return res.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Password change failed"
+      );
     }
-)
-
+  }
+);
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        loginSuccess(state, action: PayloadAction<AuthState>){
-            return {...state, ...action.payload}
-        },
-        logout(state){
-            return initialState;
-        }
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess(state, action: PayloadAction<AuthState>) {
+      return { ...state, ...action.payload };
     },
-    extraReducers: (builder) =>{
-        builder.addCase(loadUser.fulfilled, (state, action)=>{
-            state.user = action.payload.user;
-            state.isAuthenticated = action.payload.isAuthenticated;
-        });
-        builder.addCase(loadUser.rejected, (state) =>{
-            state.user = null;
-            state.isAuthenticated = false;
-        })
-        builder.addCase(changePassword.fulfilled, (state, action) =>{
-            state.passwordChangeStatus = "succeeded";
-        })
-        builder.addCase(changePassword.rejected, (state, action) => {
-            state.passwordChangeStatus = "failed";
-            state.passwordChangeError = action.payload as string;
-        });
-    }
+    logout(state) {
+      return initialState;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadUser.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.isAuthenticated = action.payload.isAuthenticated;
+    });
+    builder.addCase(loadUser.rejected, (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+    });
+    builder.addCase(changePassword.fulfilled, (state, action) => {
+      state.passwordChangeStatus = "succeeded";
+    });
+    builder.addCase(changePassword.rejected, (state, action) => {
+      state.passwordChangeStatus = "failed";
+      state.passwordChangeError = action.payload as string;
+    });
+  },
 });
 
 export const { loginSuccess, logout } = authSlice.actions;

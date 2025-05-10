e95e487a939294de6795bf9c1ce1b0443bb157e7
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-05-10 18:36:24 +0600

    Updated the footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,18 +1,27 @@
-"use client"
-import React from 'react';
-import style from './Footer.module.css';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/redux/store';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import Link from "next/link";
 
 const Footer = () => {
-    const user = useSelector((state: RootState) => state.auth.user);
+  const user = useSelector((state: RootState) => state.auth.user);
   return (
-    <div className='bg-mainBg1 flex justify-between p-3'>
-        <p>Copyright: 2025</p>
-        {user?.isAdmin && <Link href="/admin">Admin dashboard</Link>}       
+    <div className="bg-mainBg1 flex justify-between p-3">
+      <p className="flex gap-2">
+        © {new Date().getFullYear()}
+        <a
+          href="https://www.linkedin.com/in/mhasan95/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          Mahmudul Hasan
+        </a>
+      </p>
+      {user?.isAdmin && <Link href="/admin">Admin dashboard</Link>}
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

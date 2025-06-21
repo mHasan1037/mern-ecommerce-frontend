890149e31e9c27d7206f9ef069ac39218c181d05
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-06-21 19:35:57 +0600

    Fixed the category edit page

--- app/admin/category/page.tsx
+++ app/admin/category/page.tsx
@@ -4,13 +4,15 @@ import CategoryForm from "@/components/AdminProductPage/CategoryForm";
 import AdminSidebar from "@/components/adminSidebar";
 import ConfirmButton from "@/components/buttons/ConfirmButton";
 import { useAppSelector } from "@/redux/hooks";
+import { Category as CategoryType } from "@/redux/slices/categorySlice";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-
 const Category = () => {
   const [showCategoryForm, setShowCategoryForm] = useState(false);
+  const [showCategoryEditForm, setShowCategoryEditForm] = useState(false);
+  const [editCategory, setEditCategory] = useState<CategoryType | null>(null);
   const { isAuthenticated } = useAppSelector((state) => state.auth);
   const router = useRouter();
 
@@ -20,6 +22,12 @@ const Category = () => {
       toast.success("Login to your account");
     }
   }, [isAuthenticated]);
+
+  const handleEditClick = (category: CategoryType) =>{
+      setEditCategory(category)
+      setShowCategoryEditForm(true);
+      setShowCategoryForm(false);
+  }
 
   return (
     <div className="adminMainSection">
@@ -33,9 +41,19 @@ const Category = () => {
           />
         </div>
         {showCategoryForm ? (
-          <CategoryForm title="Upload Category" setShowCategoryForm={setShowCategoryForm}/>
+          <CategoryForm
+            title="Upload Category"
+            setShowCategoryForm={setShowCategoryForm}
+          />
+        ) : showCategoryEditForm && editCategory ? (
+          <CategoryForm
+            title="Edit Category"
+            setShowCategoryForm={setShowCategoryEditForm}
+            isEdit={true}
+            initialData={editCategory}
+          />
         ) : (
-          <AllCategoryList />
+          <AllCategoryList onEdit={handleEditClick}/>
         )}
       </div>
     </div>

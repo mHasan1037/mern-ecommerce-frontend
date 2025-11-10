f061f250b38d075a2c807e5497234ad36e83a33f
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-11-10 13:07:41 +0600

    Removed commented code

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -45,48 +45,6 @@ const Cart = () => {
   return (
     <div className="max-w-6xl mx-auto px-4 py-8">
       <h2 className="text-2xl font-semibold mb-6 text-center">My Cart</h2>
-      {/* <table className="table-auto w-full border border-gray-300">
-        <thead>
-          <tr className="bg-gray-100 text-left">
-            <th className="px-4 py-2 border">Image</th>
-            <th className="px-4 py-2 border">Name</th>
-            <th className="px-4 py-2 border">Price</th>
-            <th className="px-4 py-2 border">Quantity</th>
-            <th className="px-4 py-2 border"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartList
-          .map((cart) => {
-              if (!cart.product) return null;
-              return (
-                <tr key={cart._id || `${cart.product?._id}-${cart.quantity}`} className="border-t">
-                  <td className="px-4 py-2 border">
-                    {cart.product.images[0]?.url ? (
-                      <Image
-                        src={cart.product.images[0].url}
-                        alt={cart.product.name}
-                        width={80}
-                        height={80}
-                      />
-                    ) : (
-                      <span className="w-[80px] h-[80px] bg-gray-200"></span>
-                    )}
-                  </td>
-                  <td className="px-4 py-2 border">{cart.product.name}</td>
-                  <td className="px-4 py-2 border">${cart.product.price}</td>
-                  <td className="px-4 py-2 border">{cart.quantity}</td>
-                  <td
-                    className="px-4 py-2 border"
-                    onClick={() => cart.product?._id && handleDeleteCart(cart.product._id)}
-                  >
-                    X
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table> */}
       {cartList && cartList.length > 0 ? (
         <>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">

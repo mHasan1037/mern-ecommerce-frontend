99a31c8d0ecc6b4850308a1cafbee2ae6b668d70
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-11-13 15:47:38 +0600

    User cant add more product then stock in the cart

--- components/AddToCart/index.tsx
+++ components/AddToCart/index.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { addToCart, fetchCartList } from "@/redux/slices/cartSlice";
-import React, { useState } from "react";
+import { fetchProductById } from "@/redux/slices/productSlice";
+import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 interface AddToCartProps {
@@ -11,12 +12,21 @@ interface AddToCartProps {
 const AddToCart: React.FC<AddToCartProps> = ({ productId, quantity }) => {
   const dispatch = useAppDispatch();
   const { cart } = useAppSelector((state) => state.cart);
+  const {singleProduct} = useAppSelector((state) => state.products);
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(()=>{
+   dispatch(fetchProductById(productId))
+  }, [productId])
 
   const isAddedToCart = cart && cart.some((c) => c.product._id === productId);
 
   const handleAddToCart = async () => {
     if (isAddedToCart) return;
+    if(singleProduct && singleProduct?.stock < quantity){
+      toast.error(`We only have ${singleProduct?.stock} products available in the stock!`)
+      return;
+    };
     setIsLoading(true);
 
     try {

ec380bd8c982fe2265e531dbd5008a4f2be4fa0f
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2026-08-20 18:33:26 +0600

    Fixed the featured product limit issue

--- components/AdminProductPage/NewProductAdd/index.tsx
+++ components/AdminProductPage/NewProductAdd/index.tsx
@@ -1,18 +1,24 @@
 "use client";
 import AdminSidebar from "@/components/adminSidebar";
 import axiosInstance from "@/utils/axiosInstance";
-import React from "react";
 import ProductForm from "../ProductForm";
 import { ProductFormDataType } from "@/types/product";
+import { useRouter } from "next/navigation";
 
 const NewProductAdd = () => {
+  const router = useRouter();
   const handleSubmit = async (data: ProductFormDataType) => {
-    const payload = {
-      ...data,
-      price: Number(data.price),
-      stock: Number(data.stock),
-    };
-    await axiosInstance.post(`/api/upload-product`, payload);
+    try {
+      const payload = {
+        ...data,
+        price: Number(data.price),
+        stock: Number(data.stock),
+      };
+      await axiosInstance.post(`/api/upload-product`, payload);
+      router.push("/admin/product");
+    } catch (error) {
+      console.error("Failed to submit product:", error);
+    }
   };
 
   return (
